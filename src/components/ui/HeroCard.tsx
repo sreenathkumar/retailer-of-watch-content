2d88f5d8e7829869e5e1ecf87c5c11a42d55fd74
Sreenath Kumar <60534594+sreenathkumar@users.noreply.github.com>
2023-07-22 17:33:32 +0000

    fix: added styles of feature box

--- src/components/ui/HeroCard.tsx
+++ src/components/ui/HeroCard.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function HeroCard() {
   return (
     <div className='p-4 bg-blue-200 rounded-3xl'>
-      <img className='h-44 w-36' src={require('../../assets/images/logo.png')} alt="hero-cards" />
+      <img className='h-44 w-32' src={require('../../assets/images/logo.png')} alt="hero-cards" />
     </div>
   )
 }
