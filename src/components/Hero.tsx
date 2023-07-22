c4f6b713584c0cc5c1bf6f4063ed0565327b183e
Sreenath Kumar <60534594+sreenathkumar@users.noreply.github.com>
2023-07-22 16:48:10 +0000

    feat: added the animations and the bottom area

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -4,7 +4,7 @@ import HeroCard from './ui/HeroCard'
 import { gsap } from 'gsap';
 
 export default function Hero() {
-  const heroCards = React.useRef(null);
+  const hero = React.useRef(null);
 
   useLayoutEffect(() => {
 
@@ -12,36 +12,56 @@ export default function Hero() {
       // Target the two specific elements we have asigned the animate class
       gsap.to("#hero-card-1", {
         y: '360',
-        yoyo: true
+        duration: 2,
       });
 
       gsap.to("#hero-card-2", {
         y: '-360',
-        yoyo: true
+        duration: 1,
       });
 
       gsap.to("#hero-card-3", {
         y: '360',
-        yoYo: false
+        duration: 2,
       });
 
-    }, heroCards);// <- Scope!
+      gsap.to("#hero-btn", {
+        scale: 1,
+        duration: 2,
+      });
+
+      gsap.from("#hero-heading-1", {
+        y: 40,
+        duration: 2,
+      });
+
+      gsap.from("#hero-heading-2", {
+        y: 50,
+        duration: 2,
+        opacity: 0,
+      });
+      gsap.from("#hero-des", {
+        duration: 2,
+        opacity: 0,
+      });
+    }, hero);// <- Scope!
 
     return () => ctx.revert();
   }, []);
 
   return (
-    <div className='container h-screen w-full bg-slate-200 flex flex-col justify-between'>
+    <div className='container relative h-screen overflow-hidden w-full mx-auto px-6 flex flex-col justify-between' ref={hero}>
       <div className="flex justify-between grow h-full">
-        <div className="hero-texts w-1/2 bg-gray-500 flex flex-col justify-center">
-          <h1>Welcome</h1>
-          <h1>to <span>RLikes</span></h1>
-          <p>Digital Retailer of Content<br />
+        <div className="hero-texts w-1/2 bg-white flex flex-col justify-center items-start">
+          <h1 id='hero-heading-1'>Welcome</h1>
+          <h1 id='hero-heading-2' className='mb-4'>to <span>RLikes</span></h1>
+          <p id='hero-des' className='mb-6'>Digital Retailer of Content<br />
             Watch & Create Content
           </p>
+          <a href='/' id='hero-btn' className='p-4 scale-0 rounded-xl inline bg-orange-500 '>Create Account</a>
         </div>
-        <div className="w-1/2 overflow-hidden">
-          <div className="card-container flex items-center justify-center gap-10 h-full -rotate-12" ref={heroCards}>
+        <div className="card-container relative w-1/2 overflow-hidden">
+          <div className="card-wrapper bg-white flex items-center justify-center gap-10 h-full -rotate-6" >
             <div className='flex flex-col gap-10 origin-center' id='hero-card-1'>
               {
                 [1, 2, 3, 4, 5].map((item, index) => <HeroCard key={index} />)
@@ -60,8 +80,34 @@ export default function Hero() {
           </div>
         </div>
       </div>
-      <div className="hero-bottom">
-        Hero bottom
+      <div className="hero-bottom flex flex-wrap justify-between absolute px-6 bottom-14 left-0 right-0">
+        <div className="icon-box flex">
+          <div className="icon-box-icon mr-4">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="currentColor" d="M112.6 158.43a58 58 0 1 0-57.2 0a93.83 93.83 0 0 0-50.19 38.29a6 6 0 0 0 10.05 6.56a82 82 0 0 1 137.48 0a6 6 0 0 0 10-6.56a93.83 93.83 0 0 0-50.14-38.29ZM38 108a46 46 0 1 1 46 46a46.06 46.06 0 0 1-46-46Zm211 97a6 6 0 0 1-8.3-1.74A81.8 81.8 0 0 0 172 166a6 6 0 0 1 0-12a46 46 0 1 0-17.08-88.73a6 6 0 1 1-4.46-11.14a58 58 0 0 1 50.14 104.3a93.83 93.83 0 0 1 50.19 38.29A6 6 0 0 1 249 205Z" /></svg>
+          </div>
+          <div className="icon-box-texts">
+            <h2 className='text-gray-400'>Users watching</h2>
+            <p className='text-black font-bold'>1,000,000</p>
+          </div>
+        </div>
+        <div className="icon-box-box flex">
+          <div className="icon-box-icon mr-4">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="currentColor" d="M227.19 104.48A16 16 0 0 0 240 88.81V64a16 16 0 0 0-16-16H32a16 16 0 0 0-16 16v24.81a16 16 0 0 0 12.81 15.67a24 24 0 0 1 0 47A16 16 0 0 0 16 167.19V192a16 16 0 0 0 16 16h192a16 16 0 0 0 16-16v-24.81a16 16 0 0 0-12.81-15.67a24 24 0 0 1 0-47ZM32 167.2a40 40 0 0 0 0-78.39V64h56v128H32Zm192 0V192H104V64h120v24.8a40 40 0 0 0 0 78.39Z" /></svg>
+          </div>
+          <div className="icon-box-texts">
+            <h2 className='text-black font-bold'>Create Content Now</h2>
+            <p className='text-gray-400'>1,000+ available</p>
+          </div>
+        </div>
+        <div className="icon-box-box flex">
+          <div className="icon-box-icon mr-4">
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 256 256"><path fill="currentColor" d="m229.66 218.34l-50.07-50.06a88.11 88.11 0 1 0-11.31 11.31l50.06 50.07a8 8 0 0 0 11.32-11.32ZM40 112a72 72 0 1 1 72 72a72.08 72.08 0 0 1-72-72Z" /></svg>
+          </div>
+          <div className="icon-box-texts">
+            <h2 className='text-black font-bold'>Find our best creators</h2>
+            <p className='text-gray-400'>search best audio</p>
+          </div>
+        </div>
       </div>
     </div>
   )
