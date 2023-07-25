3254da56b5370cd69236f11e838b1866becbecd2
Sreenath <sreenathkumar66@gmail.com>
2023-07-26 00:31:52 +0600

    feat: products section added

---
+++ src/components/Products.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import '../assets/css/products.css'
+import ProductItem from './ui/ProductItem'
+
+export default function Products() {
+   return (
+      <div className='container px-6 py-32 mx-auto'>
+         <div className="products-headings text-center">
+            <h1 className='text-3xl font-semibold'>Over 5000+ products</h1>
+            <h2 className='text-3xl font-semibold'>available <span className='text-blue-600'>to check</span></h2>
+         </div>
+         <div className="product-card-container">
+            <ProductItem></ProductItem>
+         </div>
+      </div>
+   )
+}
