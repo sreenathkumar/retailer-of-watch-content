7aeeea970042ed9c2a040bc160d29c4fece73dfb
Sreenath Kumar <sreenathkumar66@gmail.com>
2023-08-12 22:24:24 +0600

    fix: added background shapes

--- src/components/Creators.tsx
+++ src/components/Creators.tsx
@@ -4,12 +4,28 @@ import CreatorImg from '../assets/images/creator.webp'
 
 export default function Creators() {
   return (
-    <div className='container px-6 py-32 mx-auto'>
-        <div className='flex flex-col'>
-            <span>Enjoy listening to endless</span>
-            <span>Enjoy listening to endless</span>
+    <div className='container px-6 mx-auto bg-gray-100 relative'>
+      <div className="background absolute top-0 left-0 z-10 w-full h-full">
+        <div className="shapes flex justify-around items-center h-full">
+          <div className="box-1 w-36 h-36 rounded-3xl bg-gray-200">
+           
+          </div>
+          <div className="box-2 w-56 h-56 rounded-3xl bg-gray-200">
+           
+          </div>
+          <div className="box-3 w-36 h-36 rounded-3xl bg-gray-200">
+           
+          </div>
         </div>
-        <div className='inline-block'>
+      </div>
+        <div className='flex flex-col items-center z-20 relative'>
+            <span className='text-3xl font-semibold'>Enjoy listening to endless</span>
+            <span className='text-3xl font-semibold mb-10'>stories with your creators</span>
+        </div>
+        <div className='flex justify-around z-20 relative'>
+        <CreatorCard img={CreatorImg} name='Creator'/>
+        <CreatorCard img={CreatorImg} name='Creator'/>
+        <CreatorCard img={CreatorImg} name='Creator'/>
         <CreatorCard img={CreatorImg} name='Creator'/>
         </div>
       </div>
