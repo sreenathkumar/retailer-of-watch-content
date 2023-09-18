1816709be194203d449ffff9296d31c04c7b14ed
Sreenath Kumar <sreenathkumar66@gmail.com>
2023-09-18 17:59:26 +0600

    fix: rename the logo and increase gap among menu items

--- src/components/ui/Menu.tsx
+++ src/components/ui/Menu.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Menu() {
    const menuItems: string[] = ['Home', 'About', "Contact"]
    return (
-      <ul className='flex gap-2'>
+      <ul className='flex gap-6'>
          {menuItems.map((item, index) =>
             <li key={index}>{item}</li>
          )}
