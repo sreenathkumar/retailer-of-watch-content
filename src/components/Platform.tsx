acece61d7d288e63d8d0404d809e3027882995db
Sreenath Kumar <sreenathkumar66@gmail.com>
2023-09-19 23:02:42 +0600

    fix: changed the card background color

--- src/components/Platform.tsx
+++ src/components/Platform.tsx
@@ -44,30 +44,30 @@ export default function Platform() {
     }, [])
 
     return (
-        <div id='platform-section' className='max-w-screen-xl px-6 py-10 mx-auto overflow-hidden' ref={platfrm}>
+        <div id='platform-section' className='max-w-screen-xl px-6 py-28 mx-auto overflow-hidden' ref={platfrm}>
             <div id='platform-texts' className="flex flex-col items-center max-w-md mx-auto ">
-                <h2 className='text-3xl font-semibold mb-6'>The best platform</h2>
+                <h2 className='text-3xl font-bold mb-6 .text-h'>The best platform</h2>
                 <span className='platform-des text-sm text-center font-semibold mb-10'>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta hic ut voluptas provident earum reiciendis id? Dolor optio excepturi non unde, quisquam saepe delectus iure cupiditate ea perferendis. Architecto, voluptatum.</span>
             </div>
             <div className="cards flex flex-col gap-6">
                 <div className="flex gap-6 ">
-                    <div id='platform-card-1' className='w-1/3 p-6 rounded-lg bg-pink-200 '>
+                    <div id='platform-card-1' className='w-1/3 p-6 rounded-lg bg-sky-100 '>
                         <h3 className='mb-6 text-sm'>Adaption for any devices</h3>
                         <p className='text-xl font-bold'>View Products easily with your browser!</p>
                     </div>
-                    <div id='platform-card-2' className='w-2/3 p-6 rounded-lg bg-pink-200 z-20 relative'>
+                    <div id='platform-card-2' className='w-2/3 p-6 rounded-lg bg-red-100 z-20 relative'>
                         <h3 className='mb-6 text-sm'>Stories</h3>
                         <p className='text-xl font-bold mb-10 lg:w-2/3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eos nam quos aut! Fugit commodi voluptates maiores recusandae corrupti. Quae.</p>
                         <span className='px-6 py-2 rounded-full inline-block border-2 border-black'><a href='/' rel=''>Read More</a></span>
                     </div>
                 </div>
                 <div className="flex gap-6">
-                    <div id='platform-card-3' className='w-2/3 p-6 rounded-lg bg-pink-200'>
+                    <div id='platform-card-3' className='w-2/3 p-6 rounded-lg bg-teal-100'>
                         <h3 className='mb-6 text-sm'>Stories</h3>
                         <p className='text-xl font-bold mb-10 lg:w-2/3'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nostrum eos nam quos aut! Fugit commodi voluptates maiores recusandae corrupti. Quae.</p>
                         <span className='px-6 py-2 rounded-full inline-block border-2 border-black'><a href='/' rel=''>Read More</a></span>
                     </div>
-                    <div id='platform-card-4' className='w-1/3 p-6 rounded-lg bg-pink-200'>
+                    <div id='platform-card-4' className='w-1/3 p-6 rounded-lg bg-orange-100'>
                         <h3 className='mb-6 text-sm'>Adaption for any devices</h3>
                         <p className='text-xl font-bold'>View Products easily with your browser!</p>
                     </div>
